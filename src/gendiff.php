1eb1be082f91ffc49231a518b6f962b25d88feb1
kos <konstantin000008@gmail.com>
2019-09-03 17:16:31 +0300

    step 1. add DOCOPT

--- src/gendiff.php
+++ src/gendiff.php
@@ -4,7 +4,31 @@ namespace Gendiff;
 use function \cli\prompt;
 use function \cli\line;
 
+require_once __DIR__ . '/../vendor/docopt/docopt/src/docopt.php';
+
+
 function run()
 {
+$doc = <<<'DOCOPT'
+Process FILE and optionally apply correction to either left-hand side or
+right-hand side.
+Usage: arguments_example.py [-vqrh] [FILE] ...
+       arguments_example.py (--left | --right) CORRECTION FILE
+Arguments:
+  FILE        optional input file
+  CORRECTION  correction angle, needs FILE, --left or --right to be present
+Options:
+  -h --help
+  -v       verbose mode
+  -q       quiet mode
+  -r       make report
+  --left   use left-hand side
+  --right  use right-hand side
+DOCOPT;
+
+$result = Docopt::handle($doc, array('version'=>'1.0.0rc2'));
+foreach ($result as $k=>$v){
+    echo $k.': '.json_encode($v).PHP_EOL;}
+
     line('Welcome to the Difference calculator');
 }
